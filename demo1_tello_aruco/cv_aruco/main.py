5fd1fa507b89c38e42164f3f93e361bbd1031ad0
qiyue <dbddqy@gmail.com>
2019-10-13 23:58:59 +0200

    modified main.py

--- demo1_tello_aruco/cv_aruco/main.py
+++ demo1_tello_aruco/cv_aruco/main.py
@@ -6,11 +6,12 @@ while True:
     solver.detect()
     cv2.imshow("video", solver.image)
     if solver.is_detected:
+        pose = solver.pose_world_to_tello
         print("__________")
-        print(f"x={solver.pose_world_to_tello.x}")
-        print(f"y={solver.pose_world_to_tello.y}")
-        print(f"z={solver.pose_world_to_tello.z}")
-        print(f"yaw={solver.pose_world_to_tello.yaw}")
+        print(f"x={pose.x}")
+        print(f"y={pose.y}")
+        print(f"z={pose.z}")
+        print(f"yaw={pose.yaw}")
         cv2.waitKey()
     c = cv2.waitKey(1)
     if c == ord("q"):
