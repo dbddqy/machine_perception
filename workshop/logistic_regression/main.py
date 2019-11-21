dbb8a82e1c40f1306bf6f9d4a37b4c390a7db58c
Yue <dbddqy@gmail.com>
2019-11-21 15:53:46 +0100

    logistic regression finished

--- workshop/logistic_regression/main.py
+++ workshop/logistic_regression/main.py
@@ -17,7 +17,7 @@ def generate_data(n, p=0.8):
     x = np.vstack((x1, x2))
     y = np.asarray([[1., 0.]] * n + [[0., 1.]] * n)
     # shuffle data
-    shuffle_idx = np.arange(0, n)
+    shuffle_idx = np.arange(0, n*2)
     np.random.shuffle(shuffle_idx)
     x_shuffled = x[shuffle_idx]
     y_shuffled = y[shuffle_idx]
@@ -33,11 +33,14 @@ def model(_x, _theta):
     return 1. / (1. + exp(-_x[0] * _theta[0] - _x[1] * _theta[1] - _theta[2]))
 
 
-# def loss(_x, _y, _theta, _model):
-#     _loss = 0.0
-#     for i in range(len(_x)):
-#         _loss += (model(_x[i], _theta) - _y[i]) ** 2
-#     return _loss / len(_x)
+def error_rate(_x, _y, _theta, _model):
+    error_count = 0
+    for i in range(len(_x)):
+        if (_model(_x[i], _theta) > 0.5) and _y[i][0] == 0:
+            error_count += 1;
+        if (_model(_x[i], _theta) < 0.5) and _y[i][0] == 1:
+            error_count += 1;
+    return error_count / len(_x)
 
 
 def gradient(_x, _y, _theta, _model):
@@ -49,24 +52,26 @@ def gradient(_x, _y, _theta, _model):
     return g
 
 
-def train(_x, _y, _theta, _model, lr=1e-3, steps=1000):
+def train(_x, _y, _theta, _model, lr=1e-5, steps=1000):
+    global x_test, y_test
     for step in range(steps):
         _theta -= lr * gradient(_x, _y, _theta, _model)
         # print log
         if step % 100 == 0:
-            print("step: %d, theta: %f %f %f, loss: %f" % (step, _theta[0], _theta[1], theta[2], 0.))
+            print("step: %d, theta: %f %f %f" % (step, _theta[0], _theta[1], theta[2]))
+            print("training error rate: %f, testing error rate: %f"
+                  % (error_rate(_x, _y, _theta, _model), error_rate(x_test, y_test, _theta, _model)))
     return _theta
 
 
 if __name__ == "__main__":
-    x_train, y_train, x_test, y_test = generate_data(100)
+    x_train, y_train, x_test, y_test = generate_data(300)
     theta = np.array([1., 1., 0.])
     # training
-    theta_new = train(x_train, y_train, theta, model, steps=1000)
+    theta_new = train(x_train, y_train, theta, model, steps=5000)
     # plot boundary
     x_boundary = [0., 10.]
     y_boundary = [-theta_new[2]/theta_new[1], (-theta_new[2]-10*theta_new[0])/theta_new[1]]
     plt.plot(x_boundary, y_boundary)
     plt.show()
-
 
