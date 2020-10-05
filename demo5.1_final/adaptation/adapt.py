3bd5ffe6bbde08f91f372a4bb9fc14630331ac80
Yue <dbddqy@gmail.com>
2020-10-05 19:22:12 +0200

    update final

--- demo5.1_final/adaptation/adapt.py
+++ demo5.1_final/adaptation/adapt.py
@@ -14,22 +14,22 @@ def length(v):
 
 # [7, ] [7, ] -> scalar
 def dis_l_l(l1, l2):
-    p1, v1, r1 = l1[0:3], l1[3:6], l1[6]
-    p2, v2, r2 = l2[0:3], l2[3:6], l2[6]
+    p1, v1, r1 = l1[1:4], l1[4:7], l1[0]
+    p2, v2, r2 = l2[1:4], l2[4:7], l2[0]
     cross = np.cross(v1, v2)
     return abs(np.sum((p1 - p2) * cross)) / length(cross) - r1 - r2
 
 
 # [7, ] [7, ] -> scalar
 def angle_l_l(l1, l2):
-    v1 = l1[3:6]
-    v2 = l2[3:6]
+    v1 = l1[4:7]
+    v2 = l2[4:7]
     return np.arccos(np.sum(v1*v2) / (length(v1) * length(v2)))
 
 
 # [7, ] [3, ] -> scalar
 def dis_l_t(l, t):
-    p, v = l[0:3], l[3:6]
+    p, v = l[1:4], l[4:7]
     cross = np.cross(t-p, v)
     return length(cross) / length(v)
 
@@ -38,13 +38,45 @@ def dis_l_t(l, t):
 def tran(r_6, l):
     c_new = np.zeros([7, ])
     p = np.ones([4, ])
-    p[0:3] = l[0:3]
-    v = l[3:6]
+    p[0:3] = l[1:4]
+    v = l[4:7]
     f = Frame.from_r_3(r_6[0:3], r_6[3:6].reshape([3, 1]))
-    c_new[0:3] = f.t_4_4.dot(p)[0:3].reshape([3, ])
-    c_new[3:6] = f.r_3_3.dot(v).reshape([3, ])
-    c_new[6] = l[6]
+    c_new[1:4] = f.t_4_4.dot(p)[0:3].reshape([3, ])
+    c_new[4:7] = f.r_3_3.dot(v).reshape([3, ])
+    c_new[0] = l[0]
     return c_new
+
+
+# [3, ] [3, ] -> [4, 4]
+def frame_from_normal(p, v):
+    t = np.eye(4)
+    # z axis
+    z_temp = v / length(v)
+    t[0:3, 2] = z_temp
+    # x axis
+    if abs(v[0]) < 1e-2 and abs(v[1]) < 1e-2:
+        x_temp = np.cross(v, np.array([0., 0., 1.]))
+        x_temp /= length(x_temp)
+    else:
+        x_temp = np.cross(v, np.array([0., 1., 0.]))
+        x_temp /= length(x_temp)
+    t[0:3, 0] = x_temp
+    # y axis
+    t[0:3, 1] = np.cross(z_temp, x_temp)
+    # translation
+    t[0:3, 3] = p
+    return t
+
+
+# [7, ] [7, ] -> [6, ]
+def align_l_l(l1, l2):
+    t1 = frame_from_normal(l1[1:4], l1[4:7])
+    t2 = frame_from_normal(l2[1:4], l2[4:7])
+    t21 = Frame(t2.dot(np.linalg.inv(t1)))
+    result = np.zeros([6, ])
+    result[0:3] = t21.r_3.flatten()
+    result[3:6] = t21.t_3_1.flatten()
+    return result
 
 
 with open("config/config.yml", 'r') as file:
@@ -56,8 +88,12 @@ with open("config/config.yml", 'r') as file:
 # t : target
 # ====================
 task_index = conf["task_index"]
+# is_fliped = conf["is_fliped"]
 topo = np.loadtxt(conf["path_topo"])
+pole = np.loadtxt(conf["path_design"])[task_index]
 path_result = conf["path_result"]
+path_result_pre = conf["path_result_pre"]
+path_result_target = conf["path_result_target"]
 if topo[task_index][0] == -1:
     print("doesn't need to adapt")
     sys.exit(0)
@@ -75,6 +111,13 @@ elif topo[task_index][0] == 1:
                   np.loadtxt(conf["path_m"] % (task_index, topo[task_index][4]))])
     b = np.array([np.loadtxt(conf["path_b"] % (task_index, topo[task_index][1])),
                   np.loadtxt(conf["path_b"] % (task_index, topo[task_index][4]))])
+m_init = np.zeros([7, ])
+m_init[0] = pole[0]
+m_init[1:4] = pole[1:4] + topo[task_index][2] * pole[4:7] / length(pole[4:7])
+m_init[4:7] = pole[4:7] / length(pole[4:7]) * 0.04
+pre_tran = align_l_l(m[0], m_init)
+m0 = tran(pre_tran, m[0])
+m1 = tran(pre_tran, m[1])
 
 # ===========
 # constraints
@@ -85,16 +128,16 @@ ANGLE_U = conf["angle_u"] * pi / 180.
 mu = conf["mu"]
 
 if num_cons == 1:
-    eq_cons = {"type": "eq", "fun": lambda x: dis_l_l(tran(x, m[0]), b) - DISTANCE}
-    ineq_cons = {"type": "ineq", "fun": lambda x: np.array([angle_l_l(tran(x, m[0]), b) - ANGLE_L,
-                                                            - angle_l_l(tran(x, m[0]), b) + ANGLE_U])}
+    eq_cons = {"type": "eq", "fun": lambda x: dis_l_l(tran(x, m0), b) - DISTANCE}
+    ineq_cons = {"type": "ineq", "fun": lambda x: np.array([angle_l_l(tran(x, m0), b) - ANGLE_L,
+                                                            - angle_l_l(tran(x, m0), b) + ANGLE_U])}
 if num_cons == 2:
-    eq_cons = {"type": "eq", "fun": lambda x: np.array([dis_l_l(tran(x, m[0]), b[0]) - DISTANCE,
-                                                        dis_l_l(tran(x, m[1]), b[1]) - DISTANCE])}
-    ineq_cons = {"type": "ineq", "fun": lambda x: np.array([angle_l_l(tran(x, m[0]), b[0]) - ANGLE_L,
-                                                            - angle_l_l(tran(x, m[0]), b[0]) + ANGLE_U,
-                                                            angle_l_l(tran(x, m[1]), b[1]) - ANGLE_L,
-                                                            - angle_l_l(tran(x, m[1]), b[1]) + ANGLE_U])}
+    eq_cons = {"type": "eq", "fun": lambda x: np.array([dis_l_l(tran(x, m0), b[0]) - DISTANCE,
+                                                        dis_l_l(tran(x, m1), b[1]) - DISTANCE])}
+    ineq_cons = {"type": "ineq", "fun": lambda x: np.array([angle_l_l(tran(x, m0), b[0]) - ANGLE_L,
+                                                            - angle_l_l(tran(x, m0), b[0]) + ANGLE_U,
+                                                            angle_l_l(tran(x, m1), b[1]) - ANGLE_L,
+                                                            - angle_l_l(tran(x, m1), b[1]) + ANGLE_U])}
 
 # get initial guess
 # x0 = np.zeros([6, ])
@@ -118,10 +161,12 @@ def cost(x):
     if num_cons == 2:
         return length(x)
     if num_cons == 1:
-        return dis_l_t(tran(x, m[1]), t) + mu * length(x)
+        return dis_l_t(tran(x, m1), t) + mu * length(x)
 
 
 res = minimize(cost, x0, method="SLSQP", constraints=[eq_cons, ineq_cons], options={"fto1": 1e-9, "disp": True})
 print(res.x)
 
-np.savetxt(path_result % task_index, res.x.reshape([1, 6]))
+np.savetxt(path_result_pre % task_index, pre_tran.reshape([1, 6]), fmt="%f")
+np.savetxt(path_result % task_index, res.x.reshape([1, 6]), fmt="%f")
+np.savetxt(path_result_target % task_index, tran(res.x, m1).reshape([1, 7]), fmt="%f")
